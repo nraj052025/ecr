416770956e83968519a241fcdbf3aaa8dbf54c7e
nanda30168 <nandakhishore190261@gmail.com>
2026-05-22 01:31:35 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy",
+        "message": "hello from EKS"
+    }
+
+@app.route("/")
+def root():
+    return {
+        "message": "root endpoint"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
